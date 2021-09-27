e45b15651056c13d00b990f75795d2500ca4d709
unknown <singhsunny0712@gmail.com>
2021-09-27 14:56:14 +0530

    some changes

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ if(process.env.NODE_ENV=="production"){
     app.use(express.static('client/FrontendUI/build'));
     const path=require('path')
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+        res.sendFile(path.resolve(__dirname,'client','FrontendUI','build','index.html'))
     })
 
 }
